fbfad32db77da1a4b1ca8cc98a0f8038cfc9b9d6
Douglas Almeida <doug19almeida@gmail.com>
2022-10-07 20:11:43 -0300

    Adicionando funções sócio

--- src/app/views/crud-socio/crud-socio.component.ts
+++ src/app/views/crud-socio/crud-socio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-crud-socio',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudSocioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  adicionarSocio(): void{
+    this.router.navigate(['/socio/criarSocio']);
   }
 
 }
